4339eff0ba829bb105831fe5e720acf4643b5e18
Morgan Espindola <morgan.espindola@gmail.com>
2018-04-02 17:22:45 +0100

    Version 2018-04-02 - JUnit

---
+++ Pharmacy/src/test/java/com/pharmacy/util/HibernateUtilTest.java
@@ -0,0 +1,15 @@
+package com.pharmacy.util;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+public class HibernateUtilTest {
+	
+	@Test
+	public void connect() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.close();
+		HibernateUtil.getSessionFactory().close();		
+	}
+
+}

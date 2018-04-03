b0749763ff095ec0ec5176321c3c0816e6d3deff
Morgan Espindola <morgan.espindola@gmail.com>
2018-04-03 22:03:34 +0100

    Version 2018-04-03 (Generic Domain)

---
+++ Pharmacy/src/main/java/com/pharmacy/domain/GenericDomain.java
@@ -0,0 +1,26 @@
+package com.pharmacy.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@SuppressWarnings("serial")
+@MappedSuperclass
+public class GenericDomain implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long code;
+
+	public long getCode() {
+		return code;
+	}
+
+	public void setCode(long code) {
+		this.code = code;
+	}
+
+}

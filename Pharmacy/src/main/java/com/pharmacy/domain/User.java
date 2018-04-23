5fead46160eb9bcbd3157b2586eb76eb90c2b3d8
Morgan Espindola <morgan.espindola@gmail.com>
2018-04-23 23:14:42 +0100

    User Entity

--- Pharmacy/src/main/java/com/pharmacy/domain/User.java
+++ Pharmacy/src/main/java/com/pharmacy/domain/User.java
@@ -2,14 +2,55 @@ package com.pharmacy.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class User extends GenericDomain {
-	
+
 	@Column(length = 32, nullable = false)
 	private String password;
-	
+
 	@Column(nullable = false)
 	private Character type;
+
+	@Column(nullable = false)
+	private Boolean active;
+
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Person person;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Character getType() {
+		return type;
+	}
+
+	public void setType(Character type) {
+		this.type = type;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
 
 }
